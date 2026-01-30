d41f16953548461a2df0cb64b414933474674fd6
HUMAM9797 <humam.h.mohsin@gmail.com>
2026-01-30 21:42:50 +0300

    Update working directory

--- LoanSystem.Domain/obj/Debug/net8.0/LoanSystem.Domain.AssemblyInfo.cs
+++ LoanSystem.Domain/obj/Debug/net8.0/LoanSystem.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a015c4a1ad45b334a488dacbc602cd19442f6b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
